6e662da026d8bdac76c330f4fa67914c0535341e
GKaluzhny <unholyjava@gmail.com>
2019-12-15 23:37:58 +0200

    Home-work-3 Complete project with test of entity repository with using PostgreSQL and settings in properties-files

--- src/test/java/com/coursesolvve/webproject/repository/FilmRepositoryTest.java
+++ src/test/java/com/coursesolvve/webproject/repository/FilmRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,6 +15,7 @@ import static org.junit.Assert.assertTrue;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Sql(statements = "delete from film", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@ActiveProfiles("test")
 public class FilmRepositoryTest {
 
     @Autowired
